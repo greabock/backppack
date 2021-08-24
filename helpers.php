9ee590639f0ed40214b48931dc0c55c4c2c92f45
Роман Сохарев <greabock@gmail.com>
2021-08-24 14:34:36 +0700

    сместил проверку внутрь

--- helpers.php
+++ helpers.php
@@ -2,20 +2,20 @@
 
 function findBestCombinationValue(array $products, int $maxCost = 1000, int $currentCost = 0): int
 {
-    if ($maxCost < $currentCost) {
-        return 0;
-    }
-
     return max(0, ...array_map(function (Product $product, $key) use ($currentCost, $maxCost, $products) {
-        $candidates = $products;
+        if ($maxCost >= $product->cost) {
+            $candidates = $products;
 
-        unset($candidates[$key]);
+            unset($candidates[$key]);
 
-        $product->value + findBestCombinationValue(
-            $candidates,
-            $maxCost - $product->value,
-            $currentCost + $product->cost
-        );
+            return $product->value + findBestCombinationValue(
+                    $candidates,
+                    $maxCost - $product->cost,
+                    $currentCost + $product->cost
+                );
+        }
+
+        return 0;
 
     }, $products, array_keys($products)));
 }
